4a24f163be0a05ae0a86e340d670c19580cf28ec
Michael644327 <keinsacer@gmail.com>
2024-09-30 14:08:26 +0800

    img src

--- src/pages/animalpara/funny.js
+++ src/pages/animalpara/funny.js
@@ -1,11 +1,60 @@
 import React from "react";
-const AboutPage = () => {
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Collapse from 'react-bootstrap/Collapse';
+
+import Funny from "../../components/animalpara/data/funny.json"
+
+const FunnyPage = () => {
+  const [open, setOpen] = useState(false);
 
   return (
     <>
-    <h1>Faq</h1>
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            {Funny.map((v, i) => (
+              <Button
+                onClick={() => setOpen(!open)}
+                className="accordion-button"
+                aria-controls={`collapseText${i}`}
+                data-bs-target={`#collapseText${i}`}
+                aria-expanded={open}>
+                {v.title}
+              </Button>
+            ))}
+          </h2>
+          {Funny.map((v, i) => (
+            <Collapse in={open}>
+              <div id={`collapseText${i}`} className="row">
+                <div className="col-12 col-lg-4">
+                  <img src={v.image} alt={v.title} width='100%' />
+                </div>
+                <div className="col-12 col-lg-8">
+                  {v.describe}
+                </div>
+              </div>
+            </Collapse>
+          ))}
+          <Button
+            onClick={() => setOpen(!open)}
+            aria-controls="example-collapse-text"
+            aria-expanded={open}
+          >
+            click
+          </Button>
+          <Collapse in={open}>
+            <div id="example-collapse-text">
+              Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+              terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+              labore wes anderson cred nesciunt sapiente ea proident.
+            </div>
+          </Collapse>
+        </div>
+      </div>
     </>
   )
 }
 
-export default AboutPage;
+
+export default FunnyPage;
